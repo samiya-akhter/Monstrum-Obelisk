4b8d838930fbde344d09af87a18aa084b637dd52
Nowshin <nowshin2233imtiaz@gmail.com>
2026-03-18 17:28:37 +0600

    Add files via upload

---
+++ battletower1.h
@@ -0,0 +1,20 @@
+#ifndef BATTLETOWER1PAGE_H
+#define BATTLETOWER1PAGE_H
+
+
+void drawBattleTower1()
+{
+	iShowBMP(0, 0, "Image//battletower1(1).bmp");
+}
+
+
+/*
+for back icon
+
+void backClick(int mx, int my) {
+if (mx >= 427 && mx <= 558 && my >= 57 && my <= 94) {
+gameState = 1;
+}
+}*/
+
+#endif
